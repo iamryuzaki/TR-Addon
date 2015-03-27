fcffd42ac6a5d9dba2242a7da2204222b4da8dd9
iamryuzaki <lod.skot@gmail.com>
2015-03-28 02:39:59 +0300

    Update build: 21 and HantBM

--- Interface/AddOns/Theryuzaki/Frame.lua
+++ Interface/AddOns/Theryuzaki/Frame.lua
@@ -23,9 +23,10 @@ end
 
 function A_IsCasting(name, target)
 	if not target then target = 'player'; end
-	local spell, _, _, _, _, endTime = UnitCastingInfo("target")
-	if spell then 
-		 if (not name or name == spell) then return true; end
+	local spell, _, _, _, _, endTime = UnitCastingInfo(target)
+	local spell2, _, _, _, _, endTime2 = UnitChannelInfo(target)
+	if (spell or spell2) then 
+		 if (not name or name == spell or name == spell2) then return true; end
 	end
 	return false;
 end
@@ -165,17 +166,30 @@ function Attack_1() -- ФростДК (bulid 1)
 	A_CastForTarget('Вытягивание чумы');
 end
 
-
+local A3_ManaFull = true;
 function Attack_3() -- БМ Хант (bulid 1) (by sher)
 	-- ~~~~Макросы~~~~~
 	-- 1. "/script A_Atack(3)" - Макрос для атаки вручную о при нажатии на него.
 	-- 2. "/script AutoCombo(3)" - Макрос включения автоматического режима боя.
 	-- 3. "/script DelTimeout('AutoCombo')" - Макрос для выключение автоматического режима боя.
 	-- ~~~~~~~~~~~~~~~~~
+	if (A_IsBuf('Дух ястреба') or A_IsBuf('Дух железного ястреба')) then else A_CastForTarget('Дух железного ястреба'); A_CastForTarget('Дух ястреба'); end
+	if (A_IsCasting()) then return true; end
+	if (player_mana > 95) then A3_ManaFull = true; end
+	if ((UnitMana('player') < 10 and A_IsBuf('Охотничий азарт')) or (UnitMana('player') < 30)) then A_CastForTarget('Быстрая стрельба');  A3_ManaFull = false;  end
 	A_CastForTarget('Звериный гнев');
+	A_CastForTarget('Звериный натиск');
 	A_CastForTarget('Команда "Взять!"');
+	if (target_hp <= 20) then A_CastForTarget('Убийственный выстрел'); end
 	if (A_IsDeBuf('Укус змеи', 'target', true)) then else A_CastForTarget('Укус змеи'); end
+	A_CastForTarget('Шквал');
 	if (A_IsBuf('Охотничий азарт')) then 
 		if (A_IsBuf('Удар зверя', 'pet')) then A_CastForTarget('Чародейский выстрел'); else A_CastForTarget('Залп'); end 
+	end
+	
+	if (A3_ManaFull) then
+		if ((A_IsBuf('Охотничий азарт') and player_mana >= 10) or player_mana >= 30) then A_CastForTarget('Чародейский выстрел'); end
+	else
+		A_CastForTarget('Выстрел кобры');
 	end
 end
