bd972c882ac703a732d19bb50d05ec9256a137d9
Ryuzaki <lod.skot@gmail.com>
2016-04-12 12:03:18 +0300

    Update war.lua

--- war.lua
+++ war.lua
@@ -24,3 +24,37 @@ function Attack6()
 	SetTimeout('Attack6',0.5);
 	A_Atack(6);
 end
+
+
+
+function Attack_8() 
+	if (IsDeBuf("Лунный огонь") == false)
+		SpellUse("Лунный огонь");
+	end 
+	if (IsDeBuf("Солнечный огонь") == false)
+		SpellUse("Солнечный огонь");
+	end
+	
+	if (IsBuf("Лунное затмение"))
+	    faze = 1;
+		SpellUse("Звездопад");
+		SpellUse("Звездный поток");
+		if (IsCasting()) then return; end
+		
+		SpellUse("Звездный огонь");
+		
+	end
+	if (IsBuf("Солнечное затмение"))
+	    faze = 0;
+		SpellUse("Звездный поток");
+		SpellUse("Гнев");
+	end
+	if (IsBuf("Лунное затмение") == false and IsBuf("Солнечное затмение") == false)
+	
+	    if (faze == 0) then
+			SpellUse("Гнев");
+		else
+			SpellUse("Звездный огонь");
+		end
+	end
+end
