d1f4f18ad1615af7a40bba5f6742657a5444ee46
Ryuzaki <lod.skot@gmail.com>
2016-04-12 10:30:23 +0300

    Update war.lua

--- war.lua
+++ war.lua
@@ -1,5 +1,10 @@
-function Attack_6() {
+function Attack_6() 
+	if (IsDeBuf('Удар колосса')) then
+		SpellUse("Удар громовержца");
+		SpellUse("Рев дракона");
+	end
 	if (IsBuf("Яростный выпад!")) then 
+		SpellUse("Удар колосса");
 		SpellUse("Яростный выпад");	
 	end
 	if (IsBuf("Прилив крови")) then 
@@ -14,4 +19,4 @@ function Attack_6() {
 	if (GetMana('player') >= 30) then
 		SpellUse("Зверский удар");
 	end
-}
+end
